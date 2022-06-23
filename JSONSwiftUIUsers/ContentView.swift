bcfeb97d0fbd14583bea710eb87401751ac84c57
jcastro27 <joeysk8ter@hotmail.com>
2022-06-23 14:37:47 -0500

    Initial Commit

---
+++ JSONSwiftUIUsers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JSONSwiftUIUsers
+//
+//  Created by Joseph Castro on 6/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JSONSwiftUIUsers/JSONSwiftUIUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSONSwiftUIUsersApp.swift
+//  JSONSwiftUIUsers
+//
+//  Created by Joseph Castro on 6/23/22.
+//
+
+import SwiftUI
+
+@main
+struct JSONSwiftUIUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
